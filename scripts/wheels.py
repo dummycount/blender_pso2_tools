b6d8903b090d30d7ff8220e2e0fa7dcdf6fce44f
dummycount <dummymal@hotmail.com>
2026-03-28 11:49:05 -0500

    Support Blender 5.1

--- scripts/wheels.py
+++ scripts/wheels.py
@@ -5,6 +5,7 @@ Download wheels for the project's dependencies.
 
 import shutil
 import subprocess
+from itertools import product
 from pathlib import Path
 
 ROOT = Path(__file__).parent.parent
@@ -12,14 +13,14 @@ WHEELS = ROOT / "pso2_tools" / "wheels"
 
 DEPENDENCIES = ["pythonnet==3.0.5", "watchdog==6.0.0"]
 
-PYTHON_VERSION = "3.11"
-PLATFORM = "win_amd64"
+PYTHON_VERSIONS = ["3.11", "3.13"]
+PLATFORMS = ["win_amd64"]
 
 
 def main():
     shutil.rmtree(WHEELS, ignore_errors=True)
 
-    for dep in DEPENDENCIES:
+    for dep, version, platform in product(DEPENDENCIES, PYTHON_VERSIONS, PLATFORMS):
         subprocess.call(
             [
                 "pip",
@@ -28,8 +29,8 @@ def main():
                 "--dest",
                 WHEELS,
                 "--only-binary=:all:",
-                f"--python-version={PYTHON_VERSION}",
-                f"--platform={PLATFORM}",
+                f"--python-version={version}",
+                f"--platform={platform}",
             ]
         )
 
